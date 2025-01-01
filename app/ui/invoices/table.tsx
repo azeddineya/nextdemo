9c9d0fb729cc482e6bce4a554c6bfe22eb49440b
gitName <a@b.c>
2024-12-31 23:02:14 -0500

    fix vercel deployement error - 24c601

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -47,8 +47,10 @@ export default async function InvoicesTable({
                     <p>{formatDateToLocal(invoice.date)}</p>
                   </div>
                   <div className="flex justify-end gap-2">
+                    {/*
                     <UpdateInvoice id={invoice.id} />
                     <DeleteInvoice id={invoice.id} />
+*/}
                   </div>
                 </div>
               </div>
@@ -109,8 +111,10 @@ export default async function InvoicesTable({
                   </td>
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex justify-end gap-3">
+                      {/*
                       <UpdateInvoice id={invoice.id} />
                       <DeleteInvoice id={invoice.id} />
+                      */}
                     </div>
                   </td>
                 </tr>
