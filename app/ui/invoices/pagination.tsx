f3d35197a42e8166976a5ebcb3baebbc16aed46f
gitName <a@b.c>
2024-12-31 20:33:56 -0500

    fix vercel deployment errors messages

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -3,9 +3,8 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
 
-export default function Pagination({ totalPages }: { totalPages: number }) {
+export default function Pagination() {
   // NOTE: Uncomment this code in Chapter 11
 
   // const allPages = generatePagination(currentPage, totalPages);
@@ -51,7 +50,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     </>
   );
 }
-
+/*
 function PaginationNumber({
   page,
   href,
@@ -82,7 +81,8 @@ function PaginationNumber({
     </Link>
   );
 }
-
+*/
+/*
 function PaginationArrow({
   href,
   direction,
@@ -115,5 +115,7 @@ function PaginationArrow({
     <Link className={className} href={href}>
       {icon}
     </Link>
-  );
+  ); 
 }
+  
+*/
