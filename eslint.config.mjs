7a03a8ed673e09b7a7fd9c4aab11ba1111b8fcd2
gitName <a@b.c>
2025-01-06 23:06:07 -0500

    turning off the 'error' is defined but never used

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: { "no-useless-catch": "error" }
+    rules: { '@typescript-eslint/no-unused-vars': 'off',}
   }
 ];
 
